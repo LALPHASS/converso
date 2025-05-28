0ee95188747bdb5e8d61cfb7c82ce0d4f015f8cc
Hamadoun Diarra <hamadoussdiarra@gmail.com>
2025-05-28 14:20:27 +0000

    NavBar setup

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Page = () => {
+const NewCompanion = () => {
   return (
-    <div>Page</div>
+    <div>NewCompanion</div>
   )
 }
 
-export default Page
+export default NewCompanion
